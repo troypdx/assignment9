faa8246dabab21698ca99b9951f71e7797a3704f
pdxdive <ddcarr@gmail.com>
2017-02-28 14:56:14 -0800

    checkin

--- bin/push.sh
+++ bin/push.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 # Tag, Push and Deploy only if it's not a pull request
 # Comment
-source ./bin/env.sh
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
     docker push "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
