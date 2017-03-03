991ee5d35f6d3688ee0a17dcce428c079063b441
Troy Scott <troy_pdx@fastmail.fm>
2017-03-02 21:08:50 -0800

    ecs deploy #12

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,7 @@
 export DOCKER_REPO=troypdx
 export DOCKER_IMAGE=homeless-service
 export PROJ_SETTINGS_DIR=homelessAPI
-export DEPLOY_TARGET=INTEGRATION
+export DEPLOY_TARGET=integration
 echo "##############################"
 echo  Your Local Project Environment
 echo "##############################"
--- bin/test-proj.sh
+++ bin/test-proj.sh
@@ -2,4 +2,5 @@
 source ./bin/env.sh
 # docker-compose run homeless-service python manage.py test
 # docker build -t troypdx/homeless-service:latest .
+
 docker build -t "$DOCKER_USERNAME"/"$DOCKER_IMAGE":latest .
