e7692135a75e34907936d52ce9344dd39c265cd9
Troy Scott <troy_pdx@fastmail.fm>
2017-03-02 20:53:47 -0800

    ecs deploy #10

--- bin/test-proj.sh
+++ bin/test-proj.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source ./bin/env.sh
 # docker-compose run homeless-service python manage.py test
-docker build -t troypdx/homeless-service .
+docker build -t troypdx/homeless-service:latest .
